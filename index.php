a25a24f610368c0fdbd5949081185cc597d0821c
laszlofeher <feher.laszlo.peter@gmail.com>
2024-10-15 18:44:36 +0200

    Első feltöltés

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Ez egy cím</title>
+    </head>
+    <body>
+
+        <?php
+        print('Hello');
+        
+        ?>
+        
+    </body>
+</html>
